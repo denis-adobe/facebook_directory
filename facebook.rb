2859886e46c0ace588e8b3f9267200e65707a8b7
Denis Behrends <denis-bd@outlook.de>
2014-11-07 02:48:15 +0100

    Directory Array
    Signed-off-by: Denis Behrends <denis-bd@outlook.de>

--- facebook.rb
+++ facebook.rb
@@ -7,22 +7,29 @@ https_url="https://www.facebook.com/directory/people/"
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-def open_links(new_url)
+def open_links(new_url,*array)
 
-
-  doc = Nokogiri::HTML open(new_url)
-  links = doc.css(".fbDirectoryBox a")
-  puts doc.css("title")[0].text
+      doc = Nokogiri::HTML open(new_url)
+      links = doc.css(".fbDirectoryBox a")
+      p doc.css("title")[0].text
 
 links.each do |link|
-  if !doc.at_css(".direct_listing")
-  return open_links(link["href"])
-  else
-  puts link["href"].text
-  end
+
+
+        if !doc.at_css(".direct_listing")
+              array << link["href"]
+              return open_links(link["href"],array)
+        else
+              p link["href"]
+              p array
+              File.write("log.txt",array.to_s)
+
+        end#if
+
+end#each
 
 end
 
-end
+
 
 open_links(https_url)
