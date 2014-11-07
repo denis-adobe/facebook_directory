131fded189d5f38ccda0d8ea93a99e804be7fea4
Denis Behrends <denis-bd@outlook.de>
2014-11-07 20:06:59 +0100

    elsif #Securitycheck == true pending
    Signed-off-by: Denis Behrends <denis-bd@outlook.de>

--- facebook.rb
+++ facebook.rb
@@ -4,7 +4,6 @@ require 'openssl'
 require 'rubygems'
 
 https_url="https://www.facebook.com/directory/people/"
-
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 def open_links(new_url)
@@ -13,31 +12,22 @@ def open_links(new_url)
       links = doc.css(".fbDirectoryBox a")
       p doc.css("title")[0].text
 
-
-
 links.each do |link|
-
-
-
-
 
 if !doc.at_css(".direct_listing")# Keine Nutzerliste
 open_tree = open_links(link["href"])
+#elsif # Sicherheitskontrolle
+
 else# Nutzerliste
-p link["href"]
+File.open("links.txt", 'a') { |file| file.write(link["href"] + "\n") }
 end#if
-
 end#each
-
 end#def
-
 
 def char_select(site_url)
 
 site = Nokogiri::HTML open(site_url)
 chars = site.css(".alphabet_list a")
-
-
 chars.each do |char|
 p char["href"]
 open_link = open_links(char["href"])
