5666db7df76ed3d60db56411235e20ebd686b487
denis-adobe <dns1712@googlemail.com>
2015-07-01 15:35:28 +0200

    indents and comments in facebook.rb

--- facebook.rb
+++ facebook.rb
@@ -4,40 +4,48 @@ require 'openssl'
 require 'rubygems'
 require 'mechanize'
 
-require './sqlite.rb'
+#require './sqlite.rb'
 
-https_url="https://www.facebook.com/directory/people/"
+FBDirectory="https://www.facebook.com/directory/people/"
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+
+#
+# Open Links recursively until Accountnames are reached
+#
 def open_links(new_url)
+	sleep()
 
-      doc = Nokogiri::HTML open(new_url)
-      links = doc.css(".fbDirectoryBox a")
-      p doc.css("title")[0].text
+      	doc = Nokogiri::HTML open(new_url)
+      	links = doc.css(".fbDirectoryBox a")
+      	p doc.css("title")[0].text
 
-links.each do |link|
+	links.each do |link|
 
-if !doc.at_css(".direct_listing")# Keine Nutzerliste
-open_tree = open_links(link["href"])
-#elsif # Sicherheitskontrolle
+		if !doc.at_css(".direct_listing")# Keine Nutzerliste
+			open_tree = open_links(link["href"])
+			#elsif # Sicherheitskontrolle
 
-else# Nutzerliste
-File.open("links.txt", 'a') { |file| file.write(link["href"] + "\n") }
-end#if
-end#each
+		else# Nutzerliste
+			File.open("links.txt", 'a') { |file| file.write(link["href"] + "\n") }
+		end#if
+	end#each
 end#def
 
+#
+# Select all Characters from div.alphabet_list  
+#
 def char_select(site_url)
 
-site = Nokogiri::HTML open(site_url)
-p site.at_css("title")
-chars = site.css(".alphabet_list a")
-chars.each do |char|
-p char["href"]
-open_link = open_links(char["href"])
-end#each
+	site = Nokogiri::HTML open(site_url)
+	p site.at_css("title")
+	chars = site.css(".alphabet_list a")
+	chars.each do |char|
+		p char["href"]
+		open_link = open_links(char["href"])
+	end#each
 
 end
 
 
-char_select(https_url)
+char_select(FBDirectory)
